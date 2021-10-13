ea8c3265212ae8995b3b59d23667c0e2b922a1a8
sumit chahar <chahar.sumit888@gmail.com>
2021-10-13 15:02:55 +0530

    Add changes to js file

--- block-BJaaif/code/index.js
+++ block-BJaaif/code/index.js
@@ -64,16 +64,20 @@ personStore.introduce = function(){
 function PersonConstructor() {
   this.greet = function (){
     console.log(`hello`);
+  } 
+  this.introduce = function(){
+    console.log(`Hi, my name is ${this.name}`);
   }
 }
 
 // /********* Uncomment this line to test your work! *********/
 var simon = new PersonConstructor();
-// simon.greet(); // -> Logs 'hello'
+ simon.greet(); // -> Logs 'hello'
 
 /*** CHALLENGE 2 of 3 ***/
 
 function PersonFromConstructor(name, age) {
+  PersonConstructor.call(this);
   this.name = name;
   this.age = age;
 }
@@ -81,19 +85,15 @@ function PersonFromConstructor(name, age) {
 var mike = new PersonFromConstructor('Mike', 30);
 
 // /********* Uncomment these lines to test your work! *********/
-// console.log(mike.name); // -> Logs 'Mike'
-// console.log(mike.age); //-> Logs 30
-// mike.greet(); //-> Logs 'hello'
+ console.log(mike.name); // -> Logs 'Mike'
+ console.log(mike.age); //-> Logs 30
+ mike.greet(); //-> Logs 'hello'
 
 /*** CHALLENGE 3 of 3 ***/
 // add code here
-PersonConstructor.prototype.introduce = 
-  function (){
-    console.log(`Hi, my name is ${this.name}`);
-  };
 
-Object.setPrototypeOf(PersonFromConstructor, PersonConstructor);
-// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+mike.introduce();
 
 /****************************************************************
                         USING ES6 CLASSES
@@ -147,20 +147,20 @@ function userFactory(name, score) {
   return user;
 }
 
-var adminFunctionStore = {};
-Object.setPrototypeOf(adminFunctionStore, userFunctionStore);
+var adminFunctionStore = Object.create(userFunctionStore);
 
 function adminFactory(name, score) {
   let obj = userFactory(name, score);
+  Object.setPrototypeOf(obj, adminFunctionStore);
   obj.type = 'Admin';
   return obj;
 }
 
-Object.setPrototypeOf(adminFactory, adminFunctionStore);
+
 
 /* Put code here for a method called sharePublicMessage*/
 
-adminFactory.prototype.sharedPublicMessage = function(){
+adminFunctionStore.sharedPublicMessage = function (){
   console.log(`Welcome users!`);
 }
 
